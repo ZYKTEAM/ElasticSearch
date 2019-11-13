8c7fff73746db59f5aa58f66aaa7c1faafa2ac3b
zhouyongkang <zhouyongkang>
2019-11-13 10:24:06 +0800

    提交

---
+++ src/main/java/com/example/es/service/ITbRentPriceCarModelDiaryService.java
@@ -0,0 +1,20 @@
+package com.example.es.service;
+
+import com.example.es.entity.TbRentPriceCarModelDiary;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zyk
+ * @since 2019-11-11
+ */
+public interface ITbRentPriceCarModelDiaryService  {
+
+    List<TbRentPriceCarModelDiary> finAll();
+
+
+}
